53395eaf5e25ed9efaaab087026dede5f089e35e
Guillaume Ongenae <guillaume.ongenae@algoan.com>
2021-06-11 10:41:13 +0200

    feat(events): remove handlers for algoan v1

--- src/hooks/services/hooks.service.ts
+++ src/hooks/services/hooks.service.ts
@@ -1,20 +1,9 @@
-import {
-  BanksUser,
-  BanksUserAccount,
-  BanksUserStatus,
-  EventName,
-  EventStatus,
-  PostBanksUserAccountDTO,
-  PostBanksUserTransactionDTO,
-  ServiceAccount,
-  Subscription,
-  SubscriptionEvent,
-} from '@algoan/rest';
+import { EventName, EventStatus, ServiceAccount, Subscription, SubscriptionEvent } from '@algoan/rest';
 import { Inject, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import * as delay from 'delay';
 import { isEmpty } from 'lodash';
 import * as moment from 'moment';
-import { config, Config } from 'node-config-ts';
+import { Config } from 'node-config-ts';
 
 import {
   AuthenticationResponse,
@@ -28,7 +17,6 @@ import {
   mapBridgeTransactions as mapBridgeTransactionsV2,
 } from '../../aggregator/services/bridge/bridge-v2.utils';
 import { ClientConfig } from '../../aggregator/services/bridge/bridge.client';
-import { mapBridgeAccount, mapBridgeTransactions } from '../../aggregator/services/bridge/bridge.utils';
 import {
   Account as AnalysisAccount,
   AccountTransaction as AnalysisTransaction,
@@ -41,9 +29,7 @@ import { AlgoanHttpService } from '../../algoan/services/algoan-http.service';
 import { AlgoanService } from '../../algoan/services/algoan.service';
 import { CONFIG } from '../../config/config.module';
 import { AggregatorLinkRequiredDTO } from '../dto/aggregator-link-required.dto';
-import { BankreaderLinkRequiredDTO } from '../dto/bandreader-link-required.dto';
 import { BanksDetailsRequiredDTO } from '../dto/bank-details-required.dto';
-import { BankreaderRequiredDTO } from '../dto/bankreader-required.dto';
 import { EventDTO } from '../dto/event.dto';
 
 /**
@@ -57,7 +43,7 @@ export class HooksService {
   private readonly logger: Logger = new Logger(HooksService.name);
 
   constructor(
-    @Inject(CONFIG) private readonly _config: Config,
+    @Inject(CONFIG) private readonly config: Config,
     private readonly algoanHttpService: AlgoanHttpService,
     private readonly algoanCustomerService: AlgoanCustomerService,
     private readonly algoanAnalysisService: AlgoanAnalysisService,
@@ -121,17 +107,6 @@ export class HooksService {
           await this.handleBankDetailsRequiredEvent(serviceAccount, event.payload as BanksDetailsRequiredDTO);
           break;
 
-        case EventName.BANKREADER_LINK_REQUIRED:
-          await this.handleBankreaderLinkRequiredEvent(serviceAccount, event.payload as BankreaderLinkRequiredDTO);
-          break;
-
-        // case EventName.BANKREADER_CONFIGURATION_REQUIRED:
-        //   break;
-
-        case EventName.BANKREADER_REQUIRED:
-          await this.handleBankReaderRequiredEvent(serviceAccount, event.payload as BankreaderRequiredDTO);
-          break;
-
         // The default case should never be reached, as the eventName is already checked in the DTO
         default:
           void se.update({ status: EventStatus.FAILED });
@@ -151,7 +126,7 @@ export class HooksService {
    * Handle the "aggregator_link_required" event
    * Looks for a callback URL and generates a new redirect URL
    * @param serviceAccount Concerned Algoan service account attached to the subscription
-   * @param payload Payload sent, containing the Banks User id
+   * @param payload Payload sent, containing the customer id
    */
   public async handleAggregatorLinkRequired(
     serviceAccount: ServiceAccount,
@@ -193,207 +168,10 @@ export class HooksService {
   }
 
   /**
-   * Handle the "bankreader_link_required" event
-   * Looks for a callback URL and generates a new redirect URL
-   * @param serviceAccount Concerned Algoan service account attached to the subscription
-   * @param payload Payload sent, containing the Banks User id
-   */
-  public async handleBankreaderLinkRequiredEvent(
-    serviceAccount: ServiceAccount,
-    payload: BankreaderLinkRequiredDTO,
-  ): Promise<void> {
-    let email: string | undefined;
-
-    /**
-     * 1. GET the banks user to retrieve the callback URL and the application to get the email
-     */
-    const banksUser: BanksUser = await serviceAccount.getBanksUserById(payload.banksUserId);
-    this.logger.debug({ banksUser, serviceAccount }, `Found BanksUser with id ${banksUser.id}`);
-
-    /**
-     * 1-1. If the applicationId is defined, try to get an email
-     * If the request fails, do not block the process
-     */
-    try {
-      const application = await serviceAccount.getApplicationById(payload.applicationId);
-      email = application.applicant?.contact?.email;
-    } catch (err) {
-      this.logger.warn(payload, `Application cannot be retrieved`);
-    }
-
-    /**
-     * 2. Generates a redirect URL
-     */
-    const redirectUrl: string = await this.aggregator.generateRedirectUrl(
-      banksUser.id,
-      banksUser.callbackUrl,
-      email,
-      serviceAccount.config as ClientConfig,
-    );
-
-    /**
-     * 3. Update the Banks-User, sending to Algoan the generated URL
-     */
-    await banksUser.update({
-      redirectUrl,
-    });
-
-    this.logger.debug(`Added redirect url ${banksUser.redirectUrl} to banksUser ${banksUser.id}`);
-
-    return;
-  }
-
-  /**
-   * Handle the "bankreader_required" subscription
-   * It triggers the banks accounts and transactions synchronization
-   * @param serviceAccount Concerned Algoan service account attached to the subscription
-   * @param payload Payload sent, containing the Banks User id
-   */
-  public async handleBankReaderRequiredEvent(
-    serviceAccount: ServiceAccount,
-    payload: BankreaderRequiredDTO,
-  ): Promise<void> {
-    const banksUser: BanksUser = await serviceAccount.getBanksUserById(payload.banksUserId);
-
-    /**
-     * 0. Notify Algoan that the synchronization is starting
-     */
-    await banksUser.update({
-      status: BanksUserStatus.SYNCHRONIZING,
-    });
-
-    /**
-     * 1. Retrieves an access token from Bridge to access to the user accounts
-     */
-    const authenticationResponse: AuthenticationResponse = await this.aggregator.getAccessToken(
-      banksUser.id,
-      serviceAccount.config as ClientConfig,
-    );
-    const accessToken: string = authenticationResponse.access_token;
-    const bridgeUserId: string = authenticationResponse.user.uuid;
-
-    /**
-     * 2. Retrieves Bridge banks accounts and send them to Algoan
-     */
-    const accounts: BridgeAccount[] = await this.aggregator.getAccounts(
-      accessToken,
-      serviceAccount.config as ClientConfig,
-    );
-    this.logger.debug({
-      message: `Bridge accounts retrieved for Banks User "${banksUser.id}"`,
-      accounts,
-    });
-
-    /**
-     * 2.b. Get personal information
-     */
-    let userInfo: BridgeUserInformation[] = [];
-    try {
-      userInfo = await this.aggregator.getUserPersonalInformation(accessToken, serviceAccount.config as ClientConfig);
-    } catch (err) {
-      this.logger.warn({ message: `Unable to get user personal information`, error: err });
-    }
-
-    const algoanAccounts: PostBanksUserAccountDTO[] = await mapBridgeAccount(
-      accounts,
-      userInfo,
-      accessToken,
-      this.aggregator,
-      serviceAccount.config as ClientConfig,
-    );
-    const createdAccounts: BanksUserAccount[] = await banksUser.createAccounts(algoanAccounts);
-    this.logger.debug({
-      message: `Algoan accounts created for Banks User "${banksUser.id}"`,
-      accounts: createdAccounts,
-    });
-
-    /**
-     * 3. Notify Algoan that the accounts have been synchronized
-     */
-    await banksUser.update({
-      status: BanksUserStatus.ACCOUNTS_SYNCHRONIZED,
-    });
-
-    /**
-     * 4. Retrieves Bridge transactions and send them to Algoan
-     */
-    const timeout = moment().add(config.bridge.synchronizationTimeout, 'seconds');
-    let lastUpdatedAt: string | undefined;
-    let firstFetchedDate: string;
-    let transactions: BridgeTransaction[] = [];
-    let sortedTransactions: BridgeTransaction[] = [];
-    /* eslint-disable no-magic-numbers */
-    const nbOfMonths: number = (serviceAccount.config as ClientConfig).nbOfMonths ?? 3;
-
-    do {
-      transactions = await this.aggregator.getTransactions(
-        accessToken,
-        lastUpdatedAt,
-        serviceAccount.config as ClientConfig,
-      );
-
-      for (const account of createdAccounts) {
-        const algoanTransactions: PostBanksUserTransactionDTO[] = await mapBridgeTransactions(
-          transactions.filter((transaction: BridgeTransaction) => transaction.account.id === Number(account.reference)),
-          accessToken,
-          this.aggregator,
-          serviceAccount.config as ClientConfig,
-        );
-        if (!isEmpty(algoanTransactions)) {
-          await banksUser.createTransactions(account.id, algoanTransactions);
-        }
-      }
-
-      /**
-       * Sort the transactions by updated_at
-       */
-      sortedTransactions = transactions?.sort((tr1: BridgeTransaction, tr2: BridgeTransaction) =>
-        /* eslint-disable no-magic-numbers */
-        moment(tr1.updated_at).isBefore(moment(tr2.updated_at)) ? 1 : -1,
-      );
-
-      lastUpdatedAt = sortedTransactions[0]?.updated_at ?? lastUpdatedAt;
-
-      /**
-       * Sort the transactions by date
-       */
-      sortedTransactions = transactions?.sort((tr1: BridgeTransaction, tr2: BridgeTransaction) =>
-        /* eslint-disable no-magic-numbers */
-        moment(tr1.date).isBefore(moment(tr2.date)) ? -1 : 1,
-      );
-
-      firstFetchedDate = sortedTransactions[0]?.date;
-      // Wait between each call
-      await delay(config.bridge.synchronizationWaitingTime);
-    } while (moment().diff(moment(firstFetchedDate), 'months') <= nbOfMonths && moment().isBefore(timeout));
-
-    /**
-     * 5. Notify Algoan that the process is finished
-     */
-    await banksUser.update({
-      status: BanksUserStatus.FINISHED,
-    });
-
-    /**
-     * 6. Delete the user from Bridge
-     */
-    await this.aggregator.deleteUser(
-      {
-        bridgeUserId,
-        id: banksUser.id,
-        accessToken,
-      },
-      serviceAccount.config as ClientConfig,
-    );
-
-    return;
-  }
-
-  /**
    * Handle the "bank_details_required" subscription
    * It triggers the banks accounts and transactions synchronization
    * @param serviceAccount Concerned Algoan service account attached to the subscription
-   * @param payload Payload sent, containing the Banks User id
+   * @param payload Payload sent, containing the customer id
    */
   public async handleBankDetailsRequiredEvent(
     serviceAccount: ServiceAccount,
@@ -441,8 +219,7 @@ export class HooksService {
     );
 
     // Retrieves Bridge transactions
-    // eslint-disable-next-line no-underscore-dangle
-    const timeout = moment().add(this._config.bridge.synchronizationTimeout, 'seconds');
+    const timeout = moment().add(this.config.bridge.synchronizationTimeout, 'seconds');
     let lastUpdatedAt: string | undefined;
     let transactions: BridgeTransaction[] = [];
     /* eslint-disable no-magic-numbers */
@@ -464,8 +241,7 @@ export class HooksService {
     } while (
       moment().diff(moment(transactions[0]?.date), 'months') <= nbOfMonths &&
       moment().isBefore(timeout) &&
-      // eslint-disable-next-line no-underscore-dangle
-      (await delay(this._config.bridge.synchronizationWaitingTime, { value: true }))
+      (await delay(this.config.bridge.synchronizationWaitingTime, { value: true }))
     );
 
     for (const account of algoanAccounts) {
